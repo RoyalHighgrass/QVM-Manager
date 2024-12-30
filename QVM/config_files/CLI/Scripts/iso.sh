27792c1ff35f6a6a51a0b19bb5789676960e433f
RoyalHighgrass <111022942+RoyalHighgrass@users.noreply.github.com>
2024-12-30 17:34:01 +0000

    Update iso.sh

--- QVM/config_files/CLI/Scripts/iso.sh
+++ QVM/config_files/CLI/Scripts/iso.sh
@@ -13,7 +13,8 @@ Options:\033[0m
     1. View all ISO images
     2. Download an ISO image
     3. Import ISO images
-    4. Delete an ISO image
+    4. Eject an ISO image from the QVM cdrom
+    5. Delete an ISO image
     0. Return to main menu 
 EOF
 )"
@@ -64,13 +65,16 @@ esac
 iso_search() {
     echo -e "\033[34mSearching for ISO images...\n\033[0m"
 	echo -e "\033[34mISO Images Found:\033[0m $(find ~/QVM/config_files/ISO_Images -type f -name '*.iso' | wc -l)"
-    find ~/QVM/config_files/ISO_Images -type f -name "*.iso" | cut -d"/" -f7 | nl -s'. ' -w5
+    echo -e "\033[34mISO images inside the QVM cdrom;\033[0m"
+    find ~/QVM/ -type f -name "*.iso" | cut -d"/" -f8 | grep iso | sed 's/.iso/.iso (cdrom)/g'
+    echo -e "\n\033[34mISO images stored in the QVM database;\033[0m"
+	find ~/QVM/ -type f -name "*.iso" | cut -d"/" -f7 | grep -v cdrom
 }
 
 while true; do
 	iso_menu
-	read -p "Enter a number between 0-4: " option
-	[ -z "$option" ] && echo -e "Error: Invalid entry! Operation Cancelled.\n" && exit 1
+	read -p "Enter a number between 0-5: " option
+	[ -z "$option" ] && echo -e "\033[34mError: Invalid entry! Operation Cancelled.\033[0m\n" && exit 1
 
 	case $option in
 		1)	echo -e "\033[34mListing ISO images..."
@@ -78,13 +82,8 @@ while true; do
 		;;
 		
 		2)	echo -e "\033[34mAvailable ISO images to download:\033[0m"
-	        echo -e "1. Debian 12"
-	        echo -e "2. Arch Linux"
-	        echo -e "3. Kali Linux"
-	        echo -e "4. Ubuntu-24.04"
-	        echo -e "5. RaspiOS..."
-	        echo -e "6. Other..."
-			echo -e -n "\nNote: You will need to stop the QVM application in order to cancel a download.\nEnter the number associated with your desired image (Leave blank to cancel): "
+	        echo -e "Debian-12\nArch-Linux\nKali-Linux\nUbuntu-Noble-24.04\nRaspiOS\nOther..." | nl
+			echo -e -n "\033[34m\nNote: You will need to force stop the QVM application in order to cancel a download.\nYou will also need to deleted the incomplete ISO download before trying again.\033[0m\nEnter the number associated with your desired image (Leave blank to cancel): "
 	        read iso_img
 			[ -z "$iso_img" ] && echo -e "Error: Invalid entry! Operation Cancelled.\n" && exit 1
 			iso_search
@@ -159,17 +158,39 @@ while true; do
 			done
 		;;
 		3)	echo -e "\033[34mSearching for ISO images...\033[0m"
-			if ! find ~/ -type f -name "*.iso" | grep -v "ISO Images"; then
-				echo -e "The ISO search did not find any images to import!\n"
+			if ! find $HOME -type f -name "*.iso" | grep -v "ISO_Images"; then
+				echo -e "\033[34mThe ISO search did not find any images to import!\n"
 			else
 				echo -e ""
 				echo -e "\033[34mMoving the following ISO images to the QVM ISO image folder...\033[0m"
-				find ~/ -type f -name "*.iso" | grep -v "ISO Images"
-				sudo mv $(find ~/ -type f -name "*.iso" | grep -v "ISO Images") "ISO Images"/ && \
-					echo -e "\033[34mImage(s) successfully imported!\n" || echo -e "ISO import failed!\033[0m\n"
+				find $HOME -type f -name "*.iso" | grep -v "ISO_Images"
+				sudo mv $(find ~/ -type f -name "*.iso" | grep -v "ISO_Images") "ISO_Images"/ && \
+					echo -e "\033[34mImage(s) successfully imported!\033[0m\n" || echo -e "\033[34mISO import failed!\033[0m\n"
 			fi
 		;;
-		4)	echo -e "\033[34mListing ISO images..."
+		4)	echo -e "\033[34mChecking the QVM cdrom for ISO disk images...\033[0m"
+			find $HOME/QVM/ -type f -name '*.iso' -print0 | grep -z cdrom | xargs -0 basename -a | nl -s ". "
+			read -p "Which disk image should be ejected? (Enter '0' to cancel): " si
+			[ -z $si ] && echo -e "\033[34mError: Invalid input!\033[0m" && exit 1
+			if [[ "$si" == 0 ]]; then
+				break
+			fi
+			iso=$(find $HOME/QVM/config_files/ISO_Images/cdrom/ -type f -name '*.iso' | tr -d '\0' | sed -n "${si}p")
+			while true; do
+				read -p "Eject the '$(echo $iso | xargs -0 basename -a)' disk image? [Y/n]: " eject
+				if [[ "$eject" == "Y" || "$eject" == "y" || "$eject" == "yes" ]]; then
+					sudo mv "$iso" "../ISO_Images/" && \
+					echo -e "\033[34mThe '\033[0m$(echo $iso | xargs -0 basename -a)\033[34m' has been ejected!\033[0m" || \
+					echo -e "\033[34mOperation Failed: An unexpected error has ocurred!"
+					break
+				elif [[ "$eject" == "N" || "$eject" == "n" || "$eject" == "no" ]]; then
+					break
+				else
+					echo "Error: Invalid input!"
+				fi
+			done
+		;;
+		5)	echo -e "\033[34mListing ISO images..."
 			iso_search
 			delete-iso
 		;;
