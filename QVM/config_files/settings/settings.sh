5e78001c2e510c1b548923063ed9447e4c2b9908
RoyalHighgrass <111022942+RoyalHighgrass@users.noreply.github.com>
2024-12-31 11:37:54 +0000

    Update settings.sh

--- QVM/config_files/settings/settings.sh
+++ QVM/config_files/settings/settings.sh
@@ -1,1 +1,32 @@
 
+sudo tee /usr/bin/qvm-manager > /dev/null << 'EOF'
+
+user_help="$HOME/QVM/help-info.txt"
+QVMv1="$HOME/QVM/config_files/CLI/"
+QVMv2="$HOME/QVM/config_files/GUI/"
+
+if [[ -z "$1" ]]; then
+    cd "$QVMv1"
+    pwd
+    ./qvm-manager.sh
+else
+    case "$1" in
+        --gui)
+            cd "$QVMv2" || exit
+            ./qvm-manager-gui.sh
+            ;;
+        --help|-h)
+            echo -e "$(cat "$user_help")"
+            ;;
+        --version|-v)
+            echo -e "QEMU Virtual Machine Manager v1.0.3 © QVM 2024"
+            ;;
+        *)
+            echo "Invalid option: $1"
+            ;;
+    esac
+fi
+
+EOF
+
+sudo chmod +x /usr/bin/qvm-manager
