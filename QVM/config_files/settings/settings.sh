9f9d2fc52b273fde1275e4933a965baca50e2e83
RoyalHighgrass <111022942+RoyalHighgrass@users.noreply.github.com>
2024-12-31 11:46:30 +0000

    Update settings.sh

--- QVM/config_files/settings/settings.sh
+++ QVM/config_files/settings/settings.sh
@@ -1,18 +1,23 @@
-
+mkdir $HOME/QVM
+sudo cp README.md/ $HOME/QVM/
+sudo cp QVM/* $HOME/QVM/
+mkdir $HOME/QVM/config_files/ISO_Images/
+mkdir $HOME/QVM/config_files/ISO_Images/cdrom
+mkdir $HOME/QVM/config_files/VM_Images/
+mkdir $HOME/QVM/config_files/vm_log_files/
 sudo tee /usr/bin/qvm-manager > /dev/null << 'EOF'
 
 user_help="$HOME/QVM/help-info.txt"
-QVMv1="$HOME/QVM/config_files/CLI/"
-QVMv2="$HOME/QVM/config_files/GUI/"
+QVMcli="$HOME/QVM/config_files/CLI/"
+QVMgui="$HOME/QVM/config_files/GUI/"
 
 if [[ -z "$1" ]]; then
-    cd "$QVMv1"
-    pwd
+    cd "$QVMcli"
     ./qvm-manager.sh
 else
     case "$1" in
         --gui)
-            cd "$QVMv2" || exit
+            cd "$QVMgui" || exit
             ./qvm-manager-gui.sh
             ;;
         --help|-h)
