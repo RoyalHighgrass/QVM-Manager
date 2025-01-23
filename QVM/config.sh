f523165bd5efadfd936ec627778b044076fdd339
RoyalHighgrass <111022942+RoyalHighgrass@users.noreply.github.com>
2025-01-23 10:16:36 +0000

    Update config.sh

--- QVM/config.sh
+++ QVM/config.sh
@@ -523,12 +523,13 @@ sudo chmod +x $gui/Scripts/*.sh
 sudo chmod +x $settings/*.sh
 sudo chmod -R 755 $HOME/QVM
 sudo chown -R $(whoami) $HOME/QVM
+sudo chmod +x $HOME/QVM/qvm.desktop
 sudo chmod +x $HOME/Desktop/qvm.desktop
-sudo chown -R $(whoami) $HOME/Desktop/qvm.desktop
 sudo chmod -R 755 $HOME/Desktop/qvm.desktop
+sudo chown -R $(whoami) $HOME/Desktop/qvm.desktop
 sudo chmod +x /usr/share/applications/qvm.desktop
-sudo chown -R $(whoami) /usr/share/applications/qvm.desktop
 sudo chmod -R 755 /usr/share/applications/qvm.desktop
+sudo chown -R $(whoami) /usr/share/applications/qvm.desktop
 echo -e "done!"
 
 if ! [ "$pm" = "pacman" ]; then
