fbe84c5befeddb77bd840d36a76cbdb9d1a2b5ba
RoyalHighgrass <111022942+RoyalHighgrass@users.noreply.github.com>
2025-01-24 03:47:07 +0000

    Update view-delete-vm-gui.sh

--- QVM/config_files/GUI/Scripts/view-delete-vm-gui.sh
+++ QVM/config_files/GUI/Scripts/view-delete-vm-gui.sh
@@ -30,7 +30,7 @@ if [ "$1" = "-d" ]; then
 			--button="Cancel":1 --button="OK":0
 		case $? in
 			0)	echo ""
-				yad --text-info --title="QVM-v1.0.3 - Deleting the $2 VM" --text="<b>Deleting the $2 VM! Depending on its HD size (i.e. 65GB+), it may take up to a minute or more!</b>" --button="OK":0
+				yad --title="QVM-v1.0.3 - Deleting the $2 VM" --text="<b>Deleting the $2 VM! Depending on its HD size (i.e. 65GB+), it may take up to a minute or more!</b>" --button="OK":0
 				sudo rm "${path}${2}.img"
 				sudo rm "$HOME/QVM/config_files/vm_log_files/${2}_vm_"*
 				if ! $(get_vm_info | grep "$2" &>/dev/null); then
