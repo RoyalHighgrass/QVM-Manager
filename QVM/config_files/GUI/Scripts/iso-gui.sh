5a3c0deb237541a80021f5675b887f5c4a3c7b0b
RoyalHighgrass <111022942+RoyalHighgrass@users.noreply.github.com>
2025-01-24 11:58:12 +0000

    Update iso-gui.sh

--- QVM/config_files/GUI/Scripts/iso-gui.sh
+++ QVM/config_files/GUI/Scripts/iso-gui.sh
@@ -6,7 +6,7 @@ iso_search=$(echo -e "\033[34mSearching for ISO images...\n\033[0m" && \
     find ~/QVM/ -type f -name "*.iso" | cut -d"/" -f8 | grep iso | sed 's/.iso/.iso (cdrom)/g' | sort
 )
 
-iso_menu() { yad --title "QVM-1.0.3 - Manage ISO Images " \
+iso_menu() { yad --title "QVM-1.0.3 - Manage ISO Images" \
     --form --columns=1 --width=250 --height=250 \
     --image="$HOME/QVM/config_files/logo_images/qvm-2.png" \
 	--on-top --text="QVM ISO Manager" --buttons-layout=center \
