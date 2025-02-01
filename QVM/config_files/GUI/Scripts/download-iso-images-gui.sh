92d1d71bb2c0fd7b01ee06e2e7ea36c1164d7163
RoyalHighgrass <111022942+RoyalHighgrass@users.noreply.github.com>
2025-02-01 16:18:38 +0000

    Update download-iso-images-gui.sh

--- QVM/config_files/GUI/Scripts/download-iso-images-gui.sh
+++ QVM/config_files/GUI/Scripts/download-iso-images-gui.sh
@@ -11,17 +11,12 @@ echo -e "\n${b}Select an ISO image to download...${w}"
 
 # Show the list of available ISO images using YAD
 echo -e "${b}Available ISO images;${w}"
-iso_img=$(echo -e "Debian 12 (600MB)\nArchLinux\nKali Linux\nUbuntu Noble Desktop\nUbuntu (Server)\nRaspiOS\nManjaro\nParrotOS\nFedora\nLinux Mint\nTail OS\nNone of the above (Choose Alternative)" | nl -s ".  ")
+iso_img=$(echo -e "Debian 12 (600MB)\nArchLinux (1.2GB)\nKali Linux (589MB)\nUbuntu Noble Desktop (5.8GB)\nUbuntu Server (2.6GB)\nRaspiOS (3.4GB)\nManjaro (3.6GB - 4.1GB)\nParrotOS (5.1GB)\nFedora (903MB)\nLinux Mint (2.8GB)\nNone of the above (Choose Alternative)" | nl -s ".  ")
 
 if [ "$1" = "-li" ]; then
 	echo "$iso_img"
 	exit 0
 fi
-
-get_url() {
-	local iso="$1"
-	./../settings/recommended_iso_files.sh "$iso"
-}
 
 echo "$iso_img"
 	
@@ -66,163 +61,76 @@ case $? in
 		case $selected_iso_img_num in
 	    	1)	# Pull-ISO-Debian-12-Image 
 		        if ! find $HOME/QVM/ -type f -name "*.iso" | grep "debian-12.iso" &>/dev/null; then
-	                url="https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-12.9.0-amd64-netinst.iso"
-					iso_img_nme=$(echo $url | awk -F"/" '{print $NF}')
-					
-	                confirm=$(yad --question --title="$title Confirm Download" \
-						--buttons-layout=center --on-top \
-						--text="Are you sure you want to download the official Debian 12 ISO image?" \
-						--button="Yes:0" --button="No:1")
-		            if [ $? -eq 0 ]; then
-                		if wget -c "$url" 2>&1 | sed -u 's/^/# /' | zenity --progress \
-							--title="$title Downloading ISO: Debian 12" \
-							--width=300 \
-							--text="Downloading the 'debian-12.iso' image." \
-							--percentage=0 \
-							--auto-close 2>/dev/null; then
-							zenity --info --title="$title Download Completed" \
-								--text="Debian 12 download completed successfully."  2>/dev/null && \
-							sudo mv $iso_img_nme $HOME/QVM/config_files/ISO_Images/debian-12.iso
-						else
-							zenity --error --title="$title Download Failed" \
-								--text="Debian 12 download failed." 2>/dev/null && sudo rm $iso_img_nme
-						fi
-		            fi
+					target="Debian 12"
+					url=$(./../settings/recommended_iso_files.sh "debian-12")
+					echo "$url"
+					file_name="debian-12.iso"
 		        else
-		            yad --info --text="A Debian ISO image has already been downloaded!"
+				    yad --info --text="A Debian ISO image has already been downloaded!"
+					exit 1
 	            fi
 			;;
 	    	2)	# Pull-ISO-Arch-Linux-Image 
 		        if ! find $HOME/QVM/ -type f -name "*.iso" | grep "archlinux.iso" &>/dev/null; then
-	                url="https://archlinux.mailtunnel.eu/iso/latest/archlinux-x86_64.iso"
-					iso_img_nme=$(echo $url | awk -F"/" '{print $NF}')
-	                confirm=$(yad --question --title="$title Confirm Download" \
-						--buttons-layout=center --on-top \
-						--text="Are you sure you want to download the official Arch Linux ISO image?" \
-						--button="Yes:0" --button="No:1")
-		            if [ $? -eq 0 ]; then
-						if wget -c "$url" 2>&1 | sed -u 's/^/# /' | zenity --progress \
-							--title="$title Downloading ISO: Arch Linux" \
-							--width=300 \
-						  	--text="Downloading the 'archlinux.iso' image." \
-						  	--percentage=0 \
-						  	--auto-close 2>/dev/null; then
-							zenity --info --text="Download completed successfully." 2>/dev/null && \
-							sudo mv $iso_img_nme $HOME/QVM/config_files/ISO_Images/archlinux.iso
-						else
-							zenity --error --text="Download failed." 2>/dev/null && sudo rm $iso_img_nme
-		            	fi
-					fi
+					target="Arch Linux"
+					url=$(./../settings/recommended_iso_files.sh "arch-linux")
+					echo "$url"
+					file_name="arch-linux.iso"
 		        else
-		            yad --info --text="A Arch Linux image has already been downloaded!"
+				    yad --info --text="A Arch Linux ISO image has already been downloaded!"
+					exit 1
 	            fi
 			;;
 	    	3)	# Pull-ISO-Kali-Linux-Image 
 		        if ! find $HOME/QVM/ -type f -name "*.iso" | grep "kali-linux.iso" &>/dev/null; then
-	                url="https://cdimage.kali.org/kali-2024.4/kali-linux-2024.4-installer-netinst-amd64.iso"
-					iso_img_nme=$(echo $url | awk -F"/" '{print $NF}')
-	                confirm=$(yad --question --title="$title Confirm Download" \
-						--buttons-layout=center --on-top \
-						--text="Are you sure you want to download the official Kali Linux ISO image?" \
-						--button="Yes:0" --button="No:1")
-		            if [ $? -eq 0 ]; then
-						if wget -c "$url" 2>&1 | sed -u 's/^/# /' | zenity --progress \
-							--title="$title Downloading ISO: Kali Linux" \
-							--width=300 \
-						  	--text="Downloading the 'kali-linux.iso' image." \
-						  	--percentage=0 \
-						  	--auto-close 2>/dev/null; then
-							zenity --info --text="Download completed successfully." 2>/dev/null && \
-							sudo mv $iso_img_nme $HOME/QVM/config_files/ISO_Images/kali-linux.iso 
-						else
-							zenity --error --text="Download failed." 2>/dev/null && sudo rm $iso_img_nme
-			            fi
-					fi
-				else
-		            yad --info --text="A Kali Linux image has already been downloaded!"
+					target="Kali Linux"
+					url=$(./../settings/recommended_iso_files.sh "kali-linux")
+					echo "$url"
+					file_name="kali-linux.iso"
+		        else
+				    yad --info --text="A Kali Linux ISO image has already been downloaded!"
+					exit 1
 	            fi
 			;;
 	    	4)	# Pull-ISO-Ubuntu-Noble-Image 
 		        if ! find $HOME/QVM/ -type f -name "*.iso" | grep "ubuntu-noble.iso" &>/dev/null; then
-	                url="https://releases.ubuntu.com/noble/ubuntu-24.04.1-desktop-amd64.iso"
-					iso_img_nme=$(echo $url | awk -F"/" '{print $NF}')
-	                confirm=$(yad --question --title="$title Confirm Download" \
-						--buttons-layout=center --on-top \
-						--text="Are you sure you want to download the official Ubuntu Noble ISO image?" \
-						--button="Yes:0" --button="No:1")
-		            if [ $? -eq 0 ]; then
-						if wget -c "$url" 2>&1 | sed -u 's/^/# /' | zenity --progress \
-							--title="$title Downloading ISO: Ubuntu Noble" \
-							--width=300 \
-						  	--text="Downloading the 'ubuntu-noble.iso' image." \
-						  	--percentage=0 \
-						  	--auto-close 2>/dev/null; then
-							zenity --info --text="Download completed successfully." 2>/dev/null && \
-							sudo mv $iso_img_nme $HOME/QVM/config_files/ISO_Images/ubuntu-noble.iso
-						else
-							zenity --error --text="Download failed." 2>/dev/null && sudo rm $iso_img_nme
-			            fi
-					fi
+					target="Ubuntu Noble"
+					url=$(./../settings/recommended_iso_files.sh "ubuntu-noble")
+					echo "$url"
+					file_name="ubuntu-noble.iso"
 		        else
-		            yad --info --text="A Ubuntu Noble image has already been downloaded!"
+				    yad --info --text="A Ubuntu Noble ISO image has already been downloaded!"
+					exit 1
 	            fi
 			;;
 	    	5)	# Pull-ISO-Ubuntu-Server-Image 
 		        if ! find $HOME/QVM/ -type f -name "*.iso" | grep "ubuntu-server.iso" &>/dev/null; then
-	                url="https://releases.ubuntu.com/24.04.1/ubuntu-24.04.1-live-server-amd64.iso"
-					iso_img_nme=$(echo $url | awk -F"/" '{print $NF}')
-	                confirm=$(yad --question --title="$title Confirm Download" \
-						--buttons-layout=center --on-top \
-						--text="Are you sure you want to download the official Ubuntu Server ISO image?" \
-						--button="Yes:0" --button="No:1")
-		            if [ $? -eq 0 ]; then
-						if wget -c "$url" 2>&1 | sed -u 's/^/# /' | zenity --progress \
-							--title="$title Downloading ISO: Ubuntu Server" \
-							--width=300 \
-						  	--text="Downloading the 'ubuntu-server.iso' image." \
-						  	--percentage=0 \
-						  	--auto-close 2>/dev/null; then
-							zenity --info --text="Download completed successfully." 2>/dev/null && \
-							sudo mv $iso_img_nme $HOME/QVM/config_files/ISO_Images/ubuntu-server.iso
-						else
-							zenity --error --text="Download failed." 2>/dev/null && sudo rm $iso_img_nme
-			            fi
-					fi
+					target="Ubuntu Noble"
+					url=$(./../settings/recommended_iso_files.sh "ubuntu-server")
+					echo "$url"
+					file_name="ubuntu-server.iso"
 		        else
-		            yad --info --text="A Ubuntu Server image has already been downloaded!"
+				    yad --info --text="A Ubuntu Server ISO image has already been downloaded!"
+					exit 1
 	            fi
 			;;
 	    	6)	# Pull-ISO-Raspi-OS-Image 
 		        if ! find $HOME/QVM/ -type f -name "*.iso" | grep "raspios.iso" &>/dev/null; then
-	                url="https://downloads.raspberrypi.com/rpd_x86/images/rpd_x86-2022-07-04/2022-07-01-raspios-bullseye-i386.iso"
-					iso_img_nme=$(echo $url | awk -F"/" '{print $NF}')
-	                confirm=$(yad --question --title="$title Confirm Download" \
-						--buttons-layout=center --on-top \
-						--text="Are you sure you want to download the official RaspiOS ISO image?" \
-						--button="Yes:0" --button="No:1")
-		            if [ $? -eq 0 ]; then
-						if wget -c "$url" 2>&1 | sed -u 's/^/# /' | zenity --progress \
-							--title="$title Downloading ISO: RaspiOS" \
-							--width=300 \
-						  	--text="Downloading the 'raspios.iso' image." \
-						  	--percentage=0 \
-						  	--auto-close 2>/dev/null; then
-							zenity --info --text="Download completed successfully." 2>/dev/null && \
-							sudo mv $iso_img_nme $HOME/QVM/config_files/ISO_Images/raspios.iso
-						else
-							zenity --error --text="Download failed." 2>/dev/null && sudo rm $iso_img_nme
-			            fi
-					fi
+					target="RaspiOS"
+					url=$(./../settings/recommended_iso_files.sh "raspi-os")
+					echo "$url"
+					file_name="raspi-os.iso"
 		        else
-		            yad --info --text="A RaspiOS image has already been downloaded!"
+				    yad --info --text="A Raspian ISO image has already been downloaded!"
+					exit 1
 	            fi
 			;;
 	    	7)	# Pull-ISO-Manjaro-Image 
 				echo -e "${b}Available versions of Manjaro OS;${w}"
-				manj_type=$(echo -e "Manjaro KDE Plasma Desktop\nManjaro Xfce Desktop\nManjaro GNOME Desktop" | nl -s ".  ")
-				echo $manj_type
+				manj_type=$(echo -e "Manjaro KDE Plasma Desktop (4.1GB)\nManjaro Xfce Desktop (3.6GB)\nManjaro GNOME Desktop (3.9GB)" | nl -s ".  ")
+				echo "$manj_type"
 				manj_type=$(echo "$manj_type" | yad --list --title="Available Manjaro OS images" \
-					--column="Available versions of Manjaro OS" --height=200 --width=300)
+					--column="Available versions of Manjaro OS" --height=200 --width=400)
 				
 		        if [ -z "$manj_type" ]; then
 					echo -e "Error: Operation cancelled!"
@@ -240,140 +148,71 @@ case $? in
 					version="gnome"
 				fi
 				if ! find $HOME/QVM/ -type f -name "*.iso" | grep "manjaro-${version}.iso" &>/dev/null; then
-		            case $manj_type in
-		                "KDE")
-		                    url="https://download.manjaro.org/kde/24.2.1/manjaro-kde-24.2.1-241216-linux612.iso"
-		                ;;
-		                "Xfce")
-		                    url="https://download.manjaro.org/xfce/24.2.1/manjaro-xfce-24.2.1-241216-linux612.iso"
-		                ;;
-		                "GNOME")
-		                    url="https://download.manjaro.org/gnome/24.2.1/manjaro-gnome-24.2.1-241216-linux612.iso"
-		                ;;
-		            esac
-	                iso_img_nme=$(echo $url | awk -F"/" '{print $NF}')
-	                confirm=$(yad --question --title="$title Confirm Download" \
-						--buttons-layout=center --on-top \
-						--text="Are you sure you want to download the official Manjaro $manj_type ISO image?" \
-						--button="Yes:0" --button="No:1")
-		            if [ $? -eq 0 ]; then
-						if wget -c "$url" 2>&1 | sed -u 's/^/# /' | zenity --progress \
-							--title="$title Downloading ISO: Manjaro $manj_type" \
-							--width=300 \
-						  	--text="Downloading the manjaro-${version}.iso" \
-						  	--percentage=0 \
-						  	--auto-close 2>/dev/null; then
-							zenity --info --text="Download completed successfully." 2>/dev/null && \
-							sudo mv $iso_img_nme $HOME/QVM/config_files/ISO_Images/manjaro-${version}.iso 
-						else
-							zenity --error --text="Download failed." 2>/dev/null && sudo rm $iso_img_nme
-			            fi
-					fi
+					target="Manjaro $manj_type"
+					url=$(./../settings/recommended_iso_files.sh "manjaro-${version}")
+					echo "$url"
+					file_name="manjaro-${version}.iso"
 		        else
-		            yad --info --text="A Manjaro $manj_type Desktop image has already been downloaded!"
+				    yad --info --text="A Manjaro $manj_type ISO image has already been downloaded!"
+					exit 1
 	            fi
 			;;
 	    	8)	# Pull-ISO-Parrot-OS-Image 
 		        if ! find $HOME/QVM/ -type f -name "*.iso" | grep "parrot-os.iso" &>/dev/null; then
-	                url="https://deb.parrot.sh/parrot/iso/6.2/Parrot-security-6.2_amd64.iso"
-					iso_img_nme=$(echo $url | awk -F"/" '{print $NF}')
-	                confirm=$(yad --question --title="$title Confirm Download" \
-						--buttons-layout=center --on-top \
-						--text="Are you sure you want to download the official Parrot OS ISO image?" \
-						--button="Yes:0" --button="No:1")
-		            if [ $? -eq 0 ]; then
-						if wget -c "$url" 2>&1 | sed -u 's/^/# /' | zenity --progress \
-							--title="$title Downloading ISO: Parrot OS" \
-							--width=300 \
-						  	--text="Downloading the 'parrot-os.iso' image." \
-						  	--percentage=0 \
-						  	--auto-close 2>/dev/null; then
-							zenity --info --text="Download completed successfully." 2>/dev/null && \
-							sudo mv $iso_img_nme $HOME/QVM/config_files/ISO_Images/parrot-os.iso 
-						else
-							zenity --error --text="Download failed." 2>/dev/null && sudo rm $iso_img_nme
-			            fi
-					fi
+					target="Parrot OS"
+					url=$(./../settings/recommended_iso_files.sh "parrot-os")
+					echo "$url"
+					file_name="parrot-os.iso"
 		        else
-		            yad --info --text="A Parrot OS image has already been downloaded!"
+				    yad --info --text="A Parrot OS ISO image has already been downloaded!"
+					exit 1
 	            fi
 			;;
 	    	9)	# Pull-ISO-Fedora-Image 
 		        if ! find $HOME/QVM/ -type f -name "*.iso" | grep "fedora.iso" &>/dev/null; then
-	                url="https://download.fedoraproject.org/pub/fedora/linux/releases/41/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-41-1.4.iso"
-					iso_img_nme=$(echo $url | awk -F"/" '{print $NF}')
-	                confirm=$(yad --question --title="$title Confirm Download" \
-						--buttons-layout=center --on-top \
-						--text="Are you sure you want to download the official Fedora ISO image?" \
-						--button="Yes:0" --button="No:1")
-		            if [ $? -eq 0 ]; then
-						if wget -c "$url" 2>&1 | sed -u 's/^/# /' | zenity --progress \
-							--title="$title Downloading ISO: Fedora" \
-							--width=300 \
-						  	--text="Downloading the 'fedora.iso' image." \
-						  	--percentage=0 \
-						  	--auto-close 2>/dev/null; then
-							zenity --info --text="Download completed successfully." 2>/dev/null && \
-							sudo mv $iso_img_nme $HOME/QVM/config_files/ISO_Images/fedora.iso
-						else
-							zenity --error --text="Download failed." 2>/dev/null && sudo rm $iso_img_nme
-			            fi
-					fi
+					target="Fedora"
+					url=$(./../settings/recommended_iso_files.sh "fedora")
+					echo "$url"
+					file_name="fedora.iso"
 		        else
-		            yad --info --text="A Fedora image has already been downloaded!"
+				    yad --info --text="A Fedora ISO image has already been downloaded!"
+					exit 1
 	            fi
 			;;
 	    	10)	# Pull-ISO-Linux-Mint-Image 
 		        if ! find $HOME/QVM/ -type f -name "*.iso" | grep "debian-mint.iso" &>/dev/null; then
-	                url=""
-					iso_img_nme=$(echo $url | awk -F"/" '{print $NF}')
-	                confirm=$(yad --question --title="$title Confirm Download" \
-						--buttons-layout=center --on-top \
-						--text="Are you sure you want to download the official Linux Mint ISO image?" \
-						--button="Yes:0" --button="No:1")
-		            if [ $? -eq 0 ]; then
-						if wget -c "$url" 2>&1 | sed -u 's/^/# /' | zenity --progress \
-							--title="$title Downloading ISO: Linux Mint" \
-							--width=300 \
-						  	--text="Downloading the linux-mint.iso image." \
-						  	--percentage=0 \
-						  	--auto-close 2>/dev/null; then
-							zenity --info --text="Download completed successfully." 2>/dev/null && \
-							sudo mv $iso_img_nme $HOME/QVM/config_files/ISO_Images/linux-mint.iso 
-						else
-							zenity --error --text="Download failed." 2>/dev/null && sudo rm $iso_img_nme
-			            fi
-					fi
+					target="Linux Mint"
+					url=$(./../settings/recommended_iso_files.sh "linux-mint")
+					echo "$url"
+					file_name="linux-mint.iso"
 		        else
-		            yad --info --text="A Linux Mint image has already been downloaded!"
-	            fi
-			;;
-	    	11)	# Pull-ISO-Tails-OS-Image 
-		        if ! find $HOME/QVM/ -type f -name "*.iso" | grep "tails-os.img.iso" &>/dev/null; then
-	                url="https://download.tails.net/tails/stable/tails-amd64-6.10/tails-amd64-6.10.img"
-					iso_img_nme=$(echo $url | awk -F"/" '{print $NF}')
-	                confirm=$(yad --question --title="$title Confirm Download" \
-						--buttons-layout=center --on-top \
-						--text="Are you sure you want to download the official Tails OS ISO image?" \
-						--button="Yes:0" --button="No:1")
-		            if [ $? -eq 0 ]; then
-						if wget -c "$url" 2>&1 | sed -u 's/^/# /' | zenity --progress \
-							--title="$title Downloading ISO: Tails OS" \
-							--width=300 \
-						  	--text="Downloading the 'tails-os.img.iso' image." \
-						  	--percentage=0 \
-						  	--auto-close 2>/dev/null; then
-							zenity --info --text="Download completed successfully." 2>/dev/null && \
-							sudo mv $iso_img_nme $HOME/QVM/config_files/ISO_Images/tails-os.img.iso 
-						else
-							zenity --error --text="Download failed." 2>/dev/null && sudo rm $iso_img_nme
-			            fi
-					fi
-		        else
-		            yad --info --text="A Tails OS image has already been downloaded!"
+				    yad --info --text="A Linux Mint ISO image has already been downloaded!"
+					exit 1
 	            fi
 			;;
 		esac
+		if ! [[ -z "$url" ]]; then
+			echo "$url"
+			iso_img_nme=$(basename "$url")
+	        yad --question --title="$title Confirm Download" --buttons-layout=center --on-top \
+				--text="Are you sure you want to download the official ${target} ISO image?" \
+				--button="Yes:0" --button="No:1"
+		    if [ $? -eq 0 ]; then
+                if wget -c "$url" 2>&1 | sed -u 's/^/# /' | zenity --progress \
+					--title="$title Downloading ISO: ${target}" --width=300 --percentage=0 \
+					--text="Downloading the '${target}.iso' image." --auto-close 2>/dev/null; then
+					zenity --info --title="$title Download Completed" \
+						--text="${target} download completed successfully."
+					echo "Moving $iso_img_nme to $HOME/QVM/config_files/ISO_Images/$file_name"
+					sudo mv $iso_img_nme "$HOME/QVM/config_files/ISO_Images/$file_name"
+				else
+					zenity --error --title="$title Download Failed" --text="${target} download failed."
+					sudo rm $iso_img_nme
+				fi
+		    fi
+		else
+			echo "qvm-manager: Error: An unexpected error has occured! QVM is unable to retrieve the URL for the official $target ISO image!"
+		fi
 	;;
 	1)	echo -e "Operation cancelled.\n"
 		exit 1
