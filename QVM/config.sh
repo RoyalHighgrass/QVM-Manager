a0177cafcbb7a38d1b09224a966141805b500880
RoyalHighgrass <111022942+RoyalHighgrass@users.noreply.github.com>
2025-02-03 20:14:32 +0000

    Update config.sh

--- QVM/config.sh
+++ QVM/config.sh
@@ -565,28 +565,6 @@ fi
 EOF
 echo "done!"
 
-# Create the 'Stop QVM' config file
-sudo echo "#!/bin/bash
-
-rproc=$(ps -e)
-ryp=$(echo \"$rproc\" | grep yad)
-rqp=$(echo \"$rproc\" | grep qvm)
-rzp=$(echo \"$rproc\" | grep zenity)
-rvp=$(echo \"$rproc\" | grep qemu-system)
-if ! [[ -z \"$ryp\" ]]; then
-	killall yad
-fi
-if [[ \"$rqp\" ]]; then
-	killall qvm*
-fi
-if [[ \"$rzp\" = \"zenity\" ]]; then
-	killall zenity
-fi
-if [[ \"$rvp\" = \"qemu-system*\" ]]; then
-	killall zenity
-fi
-" > $settings/stop-qvm.sh
-
 # Give all QVM files executable permissions & non-root ownership
 echo -e -n "+ qvm-manager: Configuring newly created files ... "
 sudo chmod +x /usr/bin/qvm-manager
