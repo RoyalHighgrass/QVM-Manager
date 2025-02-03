7c4b2300acdc22974ba85d1ad05fad8c2b64090f
RoyalHighgrass <111022942+RoyalHighgrass@users.noreply.github.com>
2025-02-03 21:28:14 +0000

    Update stop-qvm.sh

--- QVM/config_files/settings/stop-qvm.sh
+++ QVM/config_files/settings/stop-qvm.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 rproc=$(ps -e)
-ryp=$(echo \"$rproc\" | grep yad)
-rqp=$(echo \"$rproc\" | grep qvm)
-rzp=$(echo \"$rproc\" | grep zenity)
-rvp=$(echo \"$rproc\" | grep qemu-system)
-if ! [[ -z \"$ryp\" ]]; then
+ryp=$(echo "$rproc" | grep yad)
+rqp=$(echo "$rproc" | grep qvm)
+rzp=$(echo "$rproc" | grep zenity)
+rvp=$(echo "$rproc" | grep qemu-system)
+if ! [[ -z "$ryp" ]]; then
 	killall yad
 fi
-if [[ \"$rqp\" ]]; then
+if [[ "$rqp" ]]; then
 	killall qvm*
 fi
-if [[ \"$rzp\" = \"zenity\" ]]; then
+if [[ "$rzp" = "zenity" ]]; then
 	killall zenity
 fi
-if [[ \"$rvp\" = \"qemu-system*\" ]]; then
+if [[ "$rvp" = "qemu-system*" ]]; then
 	killall zenity
 fi
