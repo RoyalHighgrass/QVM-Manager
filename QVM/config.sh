a65684175389221134bf81e50652c3eb6c3292e7
RoyalHighgrass <111022942+RoyalHighgrass@users.noreply.github.com>
2025-02-05 23:34:34 +0000

    Update config.sh

--- QVM/config.sh
+++ QVM/config.sh
@@ -35,7 +35,7 @@ apt_dependencies="cpu-checker original-awk mawk libgtk-4-1 libgtk-3-common \
 	qemu-system-common qemu-system-arm qemu-system-x86 qemu-efi-aarch64 \
 	libsdl2-2.0-0 libsdl2-net-2.0-0 mednafen libgtk-4-dev gtk-4-examples \
 	build-essential mesa-vulkan-drivers libwebkit2gtk-4.0-doc libwebkit2gtk-4.1-0 \
-	libwebkit2gtk-4.1-dev libgtksourceview-3.0-dev libgspell-1-dev"
+	libwebkit2gtk-4.1-dev libgtksourceview-3.0-dev libgspell-1-dev grub-pc-bin"
  
 pacman_dependencies="yad gtk-layer-shell gtk3 gtk3-docs gtk3-demos gtk4 gtk4-docs gtk4-demos libportal-gtk3 \
 	libportal-gtk4 libindicator-gtk3 libvirt libvirt-dbus libvirt-glib libguestfs virt-firmware \
